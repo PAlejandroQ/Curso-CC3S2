ace0389a7e3f629919f754c76b38405670c21425
P. Alejandro Quispe <ppaabblloo4283@gmail.com>
2022-10-03 22:14:35 -0500

    Implementacion parcial de TableroGUI, Juego
    Especial enfoque en el diseño del tablero.

--- TriGO/src/main/java/org/funcionalidad/Juego.java
+++ TriGO/src/main/java/org/funcionalidad/Juego.java
@@ -1,20 +1,102 @@
 package org.funcionalidad;
 
 public class Juego {
-    static public final int numPiezasJugador = 9;
-    boolean turno;
-    protected Tablero tableroJuego;
-    protected int faseJuego;
-    public Juego()
-    {
-        turno = true;
-        tableroJuego = new Tablero();
-        faseJuego = 0;
-    }
-    public void cambioTurno(){this.turno = !this.turno;}
-    //public void checkMill(){}
+    protected static final int TOTALROWS = 7;
+    protected static final int TOTALCOLUMNS = 7;
 
-    public static void main(String[] args) {
-        System.out.println("Hello worlddddd!");
+    public enum Cell {
+        EMPTY, CROSS, NOUGHT
     }
+
+    protected Cell[][] grid;
+    protected char turn;
+
+    public enum GameState {
+        PLAYING, DRAW, CROSS_WON, NOUGHT_WON
+    }
+
+    protected GameState currentGameState;
+
+    public Juego() {
+        grid = new Cell[TOTALROWS][TOTALCOLUMNS];
+        initGame();
+    }
+
+    private void initGame() {
+        for (int row = 0; row < TOTALROWS; ++row) {
+            for (int col = 0; col < TOTALCOLUMNS; ++col) {
+                grid[row][col] = Cell.EMPTY;
+            }
+        }
+        currentGameState = GameState.PLAYING;
+        turn = 'X';
+    }
+
+    public void resetGame() {
+        initGame();
+    }
+
+    public int getTotalRows() {
+        return TOTALROWS;
+    }
+
+    public int getTotalColumns() {
+        return TOTALCOLUMNS;
+    }
+
+    public Cell getCell(int row, int column) {
+        if (row >= 0 && row < TOTALROWS && column >= 0 && column < TOTALCOLUMNS) {
+            return grid[row][column];
+        } else {
+            return null;
+        }
+    }
+
+    public char getTurn() {
+        return turn;
+    }
+
+    public void makeMove(int row, int column) {
+        if (row >= 0 && row < TOTALROWS && column >= 0 && column < TOTALCOLUMNS && grid[row][column] == Cell.EMPTY) {
+            grid[row][column] = (turn == 'X') ? Cell.CROSS : Cell.NOUGHT;
+            updateGameState(turn, row, column);
+            turn = (turn == 'X') ? 'O' : 'X';
+        }
+    }
+
+    private void updateGameState(char turn, int row, int column) {
+        if (hasWon(turn, row, column)) {
+            currentGameState = (turn == 'X') ? GameState.CROSS_WON : GameState.NOUGHT_WON;
+        } else if (isDraw()) {
+            currentGameState = GameState.DRAW;
+        }
+    }
+
+    private boolean isDraw() {
+        for (int row = 0; row < TOTALROWS; ++row) {
+            for (int col = 0; col < TOTALCOLUMNS; ++col) {
+                if (grid[row][col] == Cell.EMPTY) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean hasWon(char turn, int row, int column) {
+        Cell token = (turn == 'X') ? Cell.CROSS : Cell.NOUGHT;
+        return (grid[row][0] == token
+                && grid[row][1] == token && grid[row][2] == token
+                || grid[0][column] == token
+                && grid[1][column] == token && grid[2][column] == token
+                || row == column
+                && grid[0][0] == token && grid[1][1] == token && grid[2][2] == token
+                || row + column == 2
+                && grid[0][2] == token && grid[1][1] == token && grid[2][0] == token);
+    }
+
+    public GameState getGameState() {
+        return currentGameState;
+    }
+
 }
